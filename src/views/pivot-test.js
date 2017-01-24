c2f08c27e2002dcb6183a43ec18073518eb4414a
Aleksei <ajamkin@gmail.com>
2017-01-24 14:11:07 +0200

    simplest working variant with infiniteload and one row in header/header-column

--- src/views/pivot-test.js
+++ src/views/pivot-test.js
@@ -1,152 +1,273 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import _ from 'lodash';
-import * as actions from '../actions';
-import { MultiGrid, AutoSizer, InfiniteLoader, Grid, Table, Column, ScrollSync } from 'react-virtualized';
-import 'react-virtualized/styles.css';
-import scrollbarSize from 'dom-helpers/util/scrollbarSize';
+import React, { Component } from 'react'
+import { Grid, AutoSizer, ScrollSync, InfiniteLoader }  from 'react-virtualized'
+import shallowCompare from 'react-addons-shallow-compare'
+import cn from 'classnames'
+import scrollbarSize from 'dom-helpers/util/scrollbarSize'
 
-const list = [];
+export default class GridExample extends Component {
+  constructor(props, context) {
+    super(props, context)
 
-const remoteRowCount = 1000;
-const columnWidth = 200;
-const rowHeight = 40;
+    this.state = {
+      columnWidth: 75,
+      columnCount: 50,
+      height: 300,
+      overscanColumnCount: 0,
+      overscanRowCount: 5,
+      rowHeight: 40,
+      rowCount: 1000,
+      data: [],
+      lastLoadedRange: {}
+    };
 
-const columns = _.range(5).map(idx => ({
-  label: `col_${idx}`,
-  dataKey: `dk_col_${idx}`
-}));
-
-function isRowLoaded({ index }) {
-  //console.log('isRowLoaded', index);
-  return !!list[index];
-}
-
-function loadMoreRows({ startIndex, stopIndex }) {
-  console.log('loadMoreRows', startIndex, stopIndex);
-  return new Promise(res => {
-    for (let i = startIndex; i < stopIndex; i++) {
-      const rowData = {};
-
-      for (let j = 0; j < columns.length; j++) {
-        rowData[columns[j].dataKey] = `r${i}_c${j}`;
-      }
-
-      list[i] = rowData;
-    }
-    res(true);
-  });
-}
-
-function rowGetter({ index }) {
-  //console.log('rowGetter', index);
-  return _.get(list, index, {});
-}
-
-
-console.log(columns);
-
-class PivotTest extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this._renderHeaderCell = this._renderHeaderCell.bind(this);
-    this._renderLeftHeaderCell = this._renderLeftHeaderCell.bind(this);
+    this._renderBodyCell = this._renderBodyCell.bind(this)
+    this._renderHeaderCell = this._renderHeaderCell.bind(this)
+    this._renderLeftSideCell = this._renderLeftSideCell.bind(this)
+    this._onSectionRendered = this._onSectionRendered.bind(this);
+    this._isRowLoaded = this._isRowLoaded.bind(this);
+    this._loadMoreRows = this._loadMoreRows.bind(this);
   }
 
-  _renderHeaderCell(opts) {
-    console.log('_renderHeaderCell', opts);
-    const { columnIndex, key, rowIndex, style } = opts;
+  render() {
+    const {
+      columnCount,
+      columnWidth,
+      height,
+      overscanColumnCount,
+      overscanRowCount,
+      rowHeight,
+      rowCount
+    } = this.state
+
+    return (
+      <div className="ddp-root">
+        <main role="main">
+          <div className="container">
+            <div className="alert alert-warning" role="alert">
+              <strong>Total length:</strong>
+              &nbsp;{this.state.rowCount}
+
+              <strong style={{marginLeft:'20px'}}>Length in state:</strong>
+              &nbsp;{this.state.data.length}
+
+              <strong style={{marginLeft:'20px'}}>Last loaded range:</strong>
+              &nbsp;{JSON.stringify(this.state.lastLoadedRange)}
+            </div>
+            <ScrollSync>
+              {({ clientHeight, clientWidth, onScroll, scrollHeight, scrollLeft, scrollTop, scrollWidth }) => {
+                return (
+                  <div className='GridRow'>
+                    <div
+                      className='LeftSideGridContainer'
+                      style={{
+                        position: 'absolute',
+                        left: 0,
+                        top: 0,
+                        color: '#000',
+                        backgroundColor: `#aaa`
+                      }}
+                    >
+                      <Grid
+                        cellRenderer={this._renderLeftHeaderCell}
+                        className='HeaderGrid'
+                        width={columnWidth}
+                        height={rowHeight}
+                        rowHeight={rowHeight}
+                        columnWidth={columnWidth}
+                        rowCount={1}
+                        columnCount={1}
+                      />
+                    </div>
+                    <div
+                      className='LeftSideGridContainer'
+                      style={{
+                        position: 'absolute',
+                        left: 0,
+                        top: rowHeight,
+                        color: '#000',
+                        borderRight: '1px solid #000',
+                        backgroundColor: `#fff`
+                      }}
+                    >
+                      <Grid
+                        overscanColumnCount={overscanColumnCount}
+                        overscanRowCount={overscanRowCount}
+                        cellRenderer={this._renderLeftSideCell}
+                        columnWidth={columnWidth}
+                        columnCount={1}
+                        className='LeftSideGrid'
+                        height={height - scrollbarSize()}
+                        rowHeight={rowHeight}
+                        rowCount={rowCount}
+                        scrollTop={scrollTop}
+                        width={columnWidth}
+                      />
+                    </div>
+                    <div className='GridColumn'>
+                      <AutoSizer disableHeight>
+                        {({ width }) => (
+                          <div>
+                            <div style={{
+                              backgroundColor: `#fff`,
+                              color: '#000',
+                              borderBottom: '1px solid #000',
+                              height: rowHeight,
+                              width: width - scrollbarSize()
+                            }}>
+                              <Grid
+                                className='HeaderGrid'
+                                columnWidth={columnWidth}
+                                columnCount={columnCount}
+                                height={rowHeight}
+                                overscanColumnCount={overscanColumnCount}
+                                cellRenderer={this._renderHeaderCell}
+                                rowHeight={rowHeight}
+                                rowCount={1}
+                                scrollLeft={scrollLeft}
+                                width={width - scrollbarSize()}
+                              />
+                            </div>
+                            <div
+                              style={{
+                                backgroundColor: `#fff`,
+                                color: '#000',
+                                height,
+                                width
+                              }}
+                            >
+                              <InfiniteLoader
+                                isRowLoaded={this._isRowLoaded}
+                                loadMoreRows={this._loadMoreRows}
+                                rowCount={this.state.rowCount}
+                              >
+                                {({ onRowsRendered, registerChild }) => {
+                                  this._onRowRendered = onRowsRendered;
+                                  return (
+                                    <Grid
+                                      ref={registerChild}
+                                      onSectionRendered={this._onSectionRendered}
+                                      className='BodyGrid'
+                                      columnWidth={columnWidth}
+                                      columnCount={columnCount}
+                                      height={height}
+                                      onScroll={onScroll}
+                                      cellRenderer={this._renderBodyCell}
+                                      rowHeight={rowHeight}
+                                      rowCount={rowCount}
+                                      width={width}
+                                    />
+                                  )
+                                }}
+                              </InfiniteLoader>
+                            </div>
+                          </div>
+                        )}
+                      </AutoSizer>
+                    </div>
+                  </div>
+                )
+              }}
+            </ScrollSync>
+          </div>
+        </main>
+        <br/>
+        <br/>
+      </div>
+    )
+  }
+
+  _isRowLoaded({ index }) {
+    return !!this.state.data[index];
+  }
+
+  _loadMoreRows({ startIndex, stopIndex }) {
+    console.log('_loadMoreRows', startIndex, stopIndex);
+    const { columnCount, data } = this.state;
+    return new Promise(res => {
+      const newData = [...data];
+      for (let i = startIndex; i < stopIndex; i++) {
+        const rowData = {};
+        for (let j = 0; j < columnCount; j++) {
+          rowData[`R${i}_C${j}`] = `R${i}_C${j}`;
+        }
+        newData[i] = rowData;
+      }
+      this.setState({
+        data: newData,
+        lastLoadedRange: { startIndex, stopIndex }
+      });
+      res(true);
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return shallowCompare(this, nextProps, nextState)
+  }
+
+  _onSectionRendered({ columnStartIndex, columnStopIndex, rowStartIndex, rowStopIndex }) {
+    this._onRowRendered({
+      startIndex: rowStartIndex,
+      stopIndex: rowStopIndex,
+    });
+  }
+
+  _renderBodyCell({ columnIndex, key, rowIndex, style }) {
     if (columnIndex < 1) {
       return
     }
+
+    const rowClass = rowIndex % 2 === 0 ? 'evenRow' : 'oddRow';
+    const classNames = cn(rowClass, 'cell');
+
+    let cellData = this.state.data[rowIndex];
+
+    if (!cellData) {
+      cellData = '...'
+    } else {
+      cellData = cellData[`R${rowIndex}_C${columnIndex}`];
+    }
+
+    return (
+      <div style={style} key={key} className={classNames}>
+        {cellData}
+      </div>
+    );
+  }
+
+  _renderHeaderCell({ columnIndex, key, rowIndex, style }) {
+    if (columnIndex < 1) {
+      return
+    }
+
     return this._renderLeftHeaderCell({ columnIndex, key, rowIndex, style })
   }
 
   _renderLeftHeaderCell({ columnIndex, key, rowIndex, style }) {
     return (
-      <div key={key} className="" style={style}>
-        {`COLM${columnIndex}`}
+      <div
+        className='headerCell'
+        key={key}
+        style={style}
+      >
+        {`COL_${columnIndex}`}
       </div>
     )
   }
 
-  render() {
+  _renderLeftSideCell({ columnIndex, key, rowIndex, style }) {
+    const rowClass = rowIndex % 2 === 0
+      ? columnIndex % 2 === 0 ? 'evenRow' : 'oddRow'
+      : columnIndex % 2 !== 0 ? 'evenRow' : 'oddRow'
+    const classNames = cn(rowClass, 'cell')
+
     return (
-      <div className="ddp-root">
-        <main role="main">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="alert alert-warning" role="alert">
-                  <strong>List length: {list.length}</strong>
-                </div>
-              </div>
-            </div>
-            <ScrollSync>
-              {({ clientHeight, clientWidth, onScroll, scrollHeight, scrollLeft, scrollTop, scrollWidth }) => (
-                <div className="GridColumn">
-                  <AutoSizer disableHeight>
-                    {({ width }) => (
-                      <div>
-                        <div style={{
-                          backgroundColor: '#f9f9f9',
-                          height: rowHeight,
-                          width: width - scrollbarSize()
-                        }}>
-                          <Grid
-                            className='HeaderGrid'
-                            columnWidth={columnWidth}
-                            columnCount={_.size(columns)}
-                            height={rowHeight}
-                            cellRenderer={this._renderHeaderCell}
-                            rowHeight={rowHeight}
-                            rowCount={1}
-                            scrollLeft={scrollLeft}
-                            width={width - scrollbarSize()}
-                          />
-                        </div>
-                        <InfiniteLoader
-                          isRowLoaded={isRowLoaded}
-                          loadMoreRows={loadMoreRows}
-                          rowCount={remoteRowCount}
-                        >
-                          {({ onRowsRendered, registerChild }) => (
-                            <Table
-                              ref={registerChild}
-                              onRowsRendered={onRowsRendered}
-                              rowHeight={rowHeight}
-                              height={250}
-                              onScroll={onScroll}
-                              rowCount={remoteRowCount}
-                              rowGetter={rowGetter}
-                              width={width}
-                            >
-                              {columns.map(col => (
-                                <Column
-                                  key={col.dataKey}
-                                  width={columnWidth}
-                                  {...col}
-                                />
-                              ))}
-                            </Table>
-                          )}
-                        </InfiniteLoader>
-                      </div>
-                    )}
-                  </AutoSizer>
-                </div>
-              )}
-            </ScrollSync>
-            <br/>
-            <br/>
-          </div>
-        </main>
+      <div
+        className={classNames}
+        key={key}
+        style={style}
+      >
+        {`R${rowIndex}, C${columnIndex}`}
       </div>
     )
   }
 }
-
-export default connect(state => ({
-  comments: state.comments
-}), actions)(PivotTest);
